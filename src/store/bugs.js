0ba59b3b50f5c937f72adf81e406444626f711c4
John Grey <johngrey712@gmail.com>
2021-05-26 10:33:35 +0530

    Create Slices (combine actions and reducers) //buggy

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,36 +1,31 @@
-import { createAction, createReducer } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-/*const bugUpdated = createAction("bugUpdated"); //bugUpdated is the action type
-console.log(bugUpdated({id:1})); //payload
-console.log(bugUpdated.type); //action type*/
-
-// Action creators
-export const bugAdded = createAction("bugAdded");
-export const bugResolved = createAction("bugResolved");
-export const bugRemoved = createAction("bugRemoved");
-
-// Reducer
-// Reducer has to be default module in ducks pattern
 let lastId = 0;
 
-//createReducer(initial_state, obj that map actions to functions that handle those actions)
-export default createReducer([], {
-    //key : value
-    //actions: functions (event => event handler)
-    [bugAdded.type]: (state, action) => {
-        state.push({
-            id: ++lastId,
-            description: action.payload.description,
-            resolved: false
-        }); //immer under the hood
-    },
+const slice = createSlice({
+    name: 'bugs',
+    initialState: [],
+    reducer: {
+        bugAdded: (state, action) => {
+            state.push({
+                id: ++lastId,
+                description: action.payload.description,
+                resolved: false
+            });
+        },
 
-    [bugResolved.type]: (bugs, action) => { //can be state/bugs/any variable
-        const index = bugs.findIndex(bug =>bug.id === action.payload.id);
-        bugs[index].resolved = true;
-    },
+        bugResolved: (bugs, action) => {
+            const index = bugs.findIndex(bug =>bug.id === action.payload.id);
+            bugs[index].resolved = true;
+        },
 
-    [bugRemoved.type]: (state, action) => {
-        return state.filter(bug => bug.id !== action.payload.id)
-    }
+        bugRemoved: (state, action) => {
+            return state.filter(bug => bug.id !== action.payload.id)
+        }
+    }//maps actions to action handlers
 });
+
+console.log(slice);
+
+export const { bugAdded, bugResolved, bugRemoved } = slice.actions;
+export default slice.reducer;
