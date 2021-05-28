42465a9c64daa2081c030cb22e09589be539d9ce
John Grey <johngrey712@gmail.com>
2021-05-28 07:43:58 +0530

    Added projects slice

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import configureStore from './store/configureStore';
 import { bugAdded, bugRemoved, bugResolved } from './store/bugs';
+import { projectAdded } from './store/projects';
 
 const store = configureStore();
 
 const unscubscribe = store.subscribe(() => {
     console.log("store updated", store.getState());
 });
+
+store.dispatch(projectAdded({name: "Project 1"}));
 
 store.dispatch(bugAdded({description: "Bug 1 added"}));
 store.dispatch(bugResolved({id:1}));
