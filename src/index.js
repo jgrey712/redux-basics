bddf862d48ee7b1cae6c558a01a4feca4e36aaf5
John Grey <johngrey712@gmail.com>
2021-06-01 17:14:34 +0530

    Basic api call success

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import configureStore from './store/configureStore';
 
 const store = configureStore();
 
-/*store.dispatch((dispatch, getState) => {
-    // call  API
-    // resolved => dispatch(a)
-    dispatch({type: 'bugsReceived', bugs: [1,2,3] });
-    console.log(getState()); //put logic
-    // failed => dispatch(b)
-});*/
-
 store.dispatch({
-    type: 'error',
-    payload: {message: "an error occured"}
+    type: 'apiCallBegin',
+    payload: {
+        url: "/bugs",
+        method: "get",
+        onSuccess: "bugsReceived",
+        onError: "apiRequestFailed"
+    }
 });
